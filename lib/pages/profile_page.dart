a483d84b378befda5136ec033c5910cc944e3864
kienghuynh <63726724+kienghuynh@users.noreply.github.com>
2022-11-15 18:42:12 +0700

    profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:bepro/models/task_model.dart';
 import 'package:bepro/models/user_model.dart';
 import 'package:bepro/pages/task_page.dart.dart';
+import 'package:bepro/widget/utilities.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -40,17 +41,10 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // decoration: const BoxDecoration(
-      //   gradient: LinearGradient(colors: [
-      //     Color.fromARGB(255, 221, 181, 73),
-      //     Color.fromARGB(255, 99, 216, 204)
-      //   ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-      // ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           automaticallyImplyLeading: true,
-          
           backgroundColor: Colors.white,
           title: Center(
               child: const Text(
@@ -65,38 +59,60 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget _pageWidget() {
-    return Container(
-      child: SingleChildScrollView(
+    return SingleChildScrollView(
+      child: Container(
+        width: double.maxFinite,
+          margin: EdgeInsets.all(25),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.circular(18),
+              border: Border.all(color: Color.fromARGB(149, 194, 194, 194)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                    offset: Offset(4, 4))
+              ]),
           child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              height: 50,
+              height: 80,
             ),
-            _image('assets/person.png', 150),
-            _editButton(),
-            _textInfo('${loggedInUser.fullName}'),
+            _image(150,150),
+            SizedBox(height: 20,),
+            _textInfoName('${loggedInUser.fullName}','Họ và tên', 18, Icons.person),
             SizedBox(
-              height: 50,
+              height: 5,
             ),
-            //_liskTask(),
-            //list2()
+            Container(
+              margin: EdgeInsets.only(left:30),
+              child: Utility().BottomLine()),
+            SizedBox(height: 20,),
+            _textInfo('${loggedInUser.email}','Email:', 18, Icons.email_outlined),
+            SizedBox(height: 5,),
+            Container(
+              margin: EdgeInsets.only(left:30),
+              child: Utility().BottomLine()),
+            SizedBox(height: 30,),
+            _textInfoName('${loggedInUser.phoneNumber}','Số điện thoại', 18, Icons.phone),
+            SizedBox(height: 50,),
+            
           ],
         ),
       )),
     );
   }
 
-  Widget _image(String url, double height) {
+  Widget _image( double height, double size) {
     return Container(
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        image: DecorationImage(
-          alignment: Alignment(0, -0.8),
-          image: AssetImage('$url'),
-        ),
       ),
+      child: Icon(Icons.person, size: size,),
       height: height,
     );
   }
@@ -111,21 +127,53 @@ class _ProfilePageState extends State<ProfilePage> {
         ));
   }
 
-  Widget _textInfo(String text) {
+  Widget _textInfo(String text, String textTitle,double fontSize, IconData icon) {
     return Container(
-      child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Align(
-                alignment: Alignment.center,
-                child: Icon(
-                  Icons.person,
-                  color: Color.fromARGB(255, 99, 216, 204),
-                )),
-            Align(
-                alignment: Alignment.center,
-                child: Text('${loggedInUser.fullName}'))
-          ])),
+      width: 350,
+      height: 90,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 19),
+                child: Icon(icon,size: fontSize+6,)),
+              Container(
+                margin: EdgeInsets.only(left: 15),
+                child: Text(textTitle, style: TextStyle(fontSize: fontSize),),),
+              Container(child: SizedBox(),)
+            
+          ],
+              
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              
+              Container(
+                margin: EdgeInsets.all(15),
+                child: Text(text, style: TextStyle(fontSize: fontSize),)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _textInfoName(String text, String textTitle,double fontSize, IconData icon) {
+    return Container(
+      width: 350,
+      height: 40,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: Icon(icon,size: fontSize+6,)),
+              Expanded(flex: 2,child: Text(textTitle, style: TextStyle(fontSize: fontSize),)),
+              Expanded(flex: 3,child: Text(text, style: TextStyle(fontSize: fontSize),))]
+          ),
+        ],
+      ),
     );
   }
 
